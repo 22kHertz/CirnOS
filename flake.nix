9306d2b850edd00941437a16ef9a0d6b114f50a3
22kHertz <noah.giger@aris-space.ch>
2025-04-30 16:54:48 +0200

    bugfixing in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,14 @@
 {
-  description = "end_4's NixOS flake";
+  description = "Ricing NixOS flake";
 
-  outputs = { self, impurity, ... }: {
+  outputs = { self, impurity, nixpkgs, ... }: {
     # editing flake.nix triggers certain utilities such as direnv
     # to reload - editing host configurations do not require a direnv
     # reload, so lets move hosts out of the way
-    nixosConfigurations = import ./hosts { inherit self; };
+    nixosConfigurations = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [ /etc/nixos/configuration.nix ];
+   };
   };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
